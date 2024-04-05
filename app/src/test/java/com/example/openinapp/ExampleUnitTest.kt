edd70507e1a39d1af2a8f6a44da82390ae9a6ae5
Arsh Kumar <arshkum18@gmail.com>
2024-04-05 23:27:18 +0530

    Add documentation for ExampleUnitTest.kt

--- app/src/test/java/com/example/openinapp/ExampleUnitTest.kt
+++ app/src/test/java/com/example/openinapp/ExampleUnitTest.kt
@@ -1,17 +1,21 @@
-package com.example.openinapp
+**Folder Name: com.example.openinapp**
 
-import org.junit.Test
+**File Name: ExampleUnitTest.java**
 
-import org.junit.Assert.*
+```java
+// Folder Name: com.example.openinapp
+// File Name: ExampleUnitTest.java
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-class ExampleUnitTest {
+import org.junit.Test;
+import org.junit.Assert.*;
+
+// This class contains unit tests for the application.
+public class ExampleUnitTest {
+    // This method tests whether the addition of two numbers is correct.
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    public void addition_isCorrect() {
+        // This line asserts that the result of adding 2 and 2 is equal to 4.
+        assertEquals(4, 2 + 2);
     }
 }
+```
