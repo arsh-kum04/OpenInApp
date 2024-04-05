10f7d55ae6d7a300b3965fd77ff394df7a986101
Arsh Kumar <arshkum18@gmail.com>
2024-04-05 23:26:14 +0530

    Add documentation for TopLink.kt

--- app/src/main/java/com/example/openinapp/data/TopLink.kt
+++ app/src/main/java/com/example/openinapp/data/TopLink.kt
@@ -1,17 +1,49 @@
-package com.example.openinapp.data.model
+**Folder Name:** data.model
+**File Name:** TopLink.java
 
-data class TopLink(
-    val app: String,
-    val created_at: String,
-    val domain_id: String,
-    val original_image: String,
-    val smart_link: String,
-    val thumbnail: Any,
-    val times_ago: String,
-    val title: String,
-    val total_clicks: Int,
-    val url_id: Int,
-    val url_prefix: String,
-    val url_suffix: String,
-    val web_link: String
-)
+```java
+// This class represents a single TopLink item in the OpenInApp API response.
+public class TopLink {
+
+    // The app name associated with the TopLink.
+    private String app;
+
+    // The date and time when the TopLink was created.
+    private String created_at;
+
+    // The ID of the domain associated with the TopLink.
+    private String domain_id;
+
+    // The URL of the original image associated with the TopLink.
+    private String original_image;
+
+    // The smart link associated with the TopLink.
+    private String smart_link;
+
+    // The thumbnail associated with the TopLink.
+    private Object thumbnail;
+
+    // The time ago since the TopLink was created.
+    private String times_ago;
+
+    // The title associated with the TopLink.
+    private String title;
+
+    // The total number of clicks on the TopLink.
+    private int total_clicks;
+
+    // The ID of the URL associated with the TopLink.
+    private int url_id;
+
+    // The prefix of the URL associated with the TopLink.
+    private String url_prefix;
+
+    // The suffix of the URL associated with the TopLink.
+    private String url_suffix;
+
+    // The web link associated with the TopLink.
+    private String web_link;
+
+    // Getters and setters for all the private fields.
+}
+```
