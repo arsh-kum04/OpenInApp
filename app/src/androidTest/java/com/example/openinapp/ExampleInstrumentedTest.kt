2f1e8b760ed6e4da14361c77bbcae36f1f57ac9a
Arsh Kumar <arshkum18@gmail.com>
2024-04-05 23:24:48 +0530

    Add documentation for ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/example/openinapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/openinapp/ExampleInstrumentedTest.kt
@@ -1,24 +1,8 @@
-package com.example.openinapp
-
-import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
-
-import org.junit.Test
-import org.junit.runner.RunWith
-
-import org.junit.Assert.*
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-@RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
-    @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.openinapp", appContext.packageName)
-    }
-}
+1. Folder Name: com.example.openinapp
+2. File Name: ExampleInstrumentedTest.java
+```java
+// Context of the app under test.
+val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+// Assertions are used to test conditions in a unit test.  If the condition is false, the test fails.
+assertEquals("com.example.openinapp", appContext.packageName)
+```
