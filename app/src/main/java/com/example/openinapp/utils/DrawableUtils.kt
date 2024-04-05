2000a4d540a805b4966b9af21758096530ea6382
Arsh Kumar <arshkum18@gmail.com>
2024-04-05 23:26:57 +0530

    Add documentation for DrawableUtils.kt

--- app/src/main/java/com/example/openinapp/utils/DrawableUtils.kt
+++ app/src/main/java/com/example/openinapp/utils/DrawableUtils.kt
@@ -1,12 +1,21 @@
-package com.example.openinapp.utils
+**Folder Name:** com.example.openinapp.utils
 
-import android.graphics.drawable.GradientDrawable
+**File Name:** DrawableUtils.kt
 
+```kotlin
+// Folder Name: com.example.openinapp.utils
+// File Name: DrawableUtils.kt
+
+// This class contains utility functions for creating gradient drawables.
 object DrawableUtils {
+    // Function to create a gradient drawable with the specified start and end colors.
     fun createGradientDrawable(startColor: Int, endColor: Int): GradientDrawable {
+        // Create a new GradientDrawable with a top-to-bottom orientation.
         return GradientDrawable(
             GradientDrawable.Orientation.TOP_BOTTOM,
+            // Set the colors of the gradient.
             intArrayOf(startColor, endColor)
         )
     }
 }
+```
