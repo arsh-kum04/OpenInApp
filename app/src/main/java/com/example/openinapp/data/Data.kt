54addb99d786c15633b95fa069fe620236c4a707
Arsh Kumar <arshkum18@gmail.com>
2024-04-05 23:25:44 +0530

    Add documentation for Data.kt

--- app/src/main/java/com/example/openinapp/data/Data.kt
+++ app/src/main/java/com/example/openinapp/data/Data.kt
@@ -1,10 +1,27 @@
-package com.example.openinapp.data
+```java
+// Folder Name: data
+// File Name: Data.java
 
-import com.example.openinapp.data.model.RecentLink
-import com.example.openinapp.data.model.TopLink
+import com.example.openinapp.data.model.RecentLink;
+import com.example.openinapp.data.model.TopLink;
 
-data class Data(
-    val overall_url_chart: Map<String, Int>,
-    val recent_links: MutableList<RecentLink>,
-    val top_links: MutableList<TopLink>
+/**
+ * Data class to hold the overall URL chart, recent links, and top links.
+ */
+public data class Data(
+    /**
+     * Map of URL to the number of times it has been opened.
+     */
+    private val overallUrlChart: Map<String, Int>,
+
+    /**
+     * List of recent links.
+     */
+    private val recentLinks: MutableList<RecentLink>,
+
+    /**
+     * List of top links.
+     */
+    private val topLinks: MutableList<TopLink>
 )
+```
