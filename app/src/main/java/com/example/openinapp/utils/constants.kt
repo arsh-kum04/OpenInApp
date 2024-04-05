75a7e7f1400d40cfb5bf324662fad9cc58a54076
Arsh Kumar <arshkum18@gmail.com>
2024-04-05 23:27:03 +0530

    Add documentation for constants.kt

--- app/src/main/java/com/example/openinapp/utils/constants.kt
+++ app/src/main/java/com/example/openinapp/utils/constants.kt
@@ -1,6 +1,15 @@
+---
+```java
+// Folder Name: utils
+// File Name: Constants.java
+
 package com.example.openinapp.utils
 
 object constants {
-    const val BASE_URL="https://api.inopenapp.com/"
+    // This variable stores the base URL for API requests.
+    const val BASE_URL = "https://api.inopenapp.com/"
+    
+    // This variable stores the authorization token used for API requests.
     const val token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjU5MjcsImlhdCI6MTY3NDU1MDQ1MH0.dCkW0ox8tbjJA2GgUx2UEwNlbTZ7Rr38PVFJevYcXFI"
 }
+```
