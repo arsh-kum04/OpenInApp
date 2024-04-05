5149b8a1ceab0b8d7ce7e83972dc5890f18c6d49
Arsh Kumar <arshkum18@gmail.com>
2024-04-05 23:25:31 +0530

    Add documentation for OpenInAppAPI.kt

--- app/src/main/java/com/example/openinapp/apis/OpenInAppAPI.kt
+++ app/src/main/java/com/example/openinapp/apis/OpenInAppAPI.kt
@@ -1,11 +1,16 @@
-package com.example.openinapp.apis
-
+```python
+# Folder Name: apis
+# File Name: OpenInAppAPI.py
+# Line by line documented Code:
 import com.example.openinapp.data.DataResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Header
 
 interface OpenInAppAPI {
+
+    # This function gets data from the server.
     @GET("api/v1/dashboardNew")
     fun getData(@Header("Authorization") token: String): Call<DataResponse>
 }
+```
